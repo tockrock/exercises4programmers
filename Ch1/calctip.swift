ebcc98c1e5052cad4d7d5f20b2798456e5a93a83
Takeshi Suzuki <tockrock@gmail.com>
2021-05-06 11:01:38 +0900

    MVP

--- Ch1/calctip.swift
+++ Ch1/calctip.swift
@@ -7,6 +7,28 @@
 //
 import Foundation
 
-print("What's your name?")
-let name = readLine() ?? "World"
-print("Hello \(name)!")
+
+print("What is the amount?")
+let priceInput = readLine()
+print("What is the percentage for the tip?")
+let tipInput = readLine()
+
+//guard let amount = amount else {
+//    print("Needs amount.")
+//}
+//guard let tip = tip else {
+//    print("Needs Tip.")
+//}
+//
+
+let price = Double(priceInput!)
+let tip = Double(tipInput!)
+
+
+let tipAmount = price! * tip! * 0.01
+let total = price! + tipAmount
+
+print("tip is \(tipAmount)")
+print("total  is \(total)")
+
+

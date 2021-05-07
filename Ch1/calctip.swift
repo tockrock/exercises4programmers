4e79de742abb3ea2a95f17df013b7c236ea74d5c
Takeshi Suzuki <tockrock@gmail.com>
2021-05-08 08:11:37 +0900

    put everything in a function for now

--- Ch1/calctip.swift
+++ Ch1/calctip.swift
@@ -7,12 +7,6 @@
 //
 import Foundation
 
-
-print("What is the amount?")
-let priceInput = readLine()
-print("What is the percentage for the tip?")
-let tipInput = readLine()
-
 //guard let amount = amount else {
 //    print("Needs amount.")
 //}
@@ -21,14 +15,20 @@ let tipInput = readLine()
 //}
 //
 
-let price = Double(priceInput!)
-let tip = Double(tipInput!)
+func main() {
+    print("What is the amount?")
+    let priceInput = readLine() ?? ""
+    print("What is the percentage for the tip?")
+    let tipInput = readLine() ?? ""
 
+    let price = Double(priceInput)
+    let tip = Double(tipInput)
 
-let tipAmount = price! * tip! * 0.01
-let total = price! + tipAmount
+    let tipAmount = price! * tip! * 0.01
+    let total = price! + tipAmount
 
-print("tip is \(tipAmount)")
-print("total  is \(total)")
+    print("tip is \(tipAmount)")
+    print("total  is \(total)")
+}
 
-
+main()

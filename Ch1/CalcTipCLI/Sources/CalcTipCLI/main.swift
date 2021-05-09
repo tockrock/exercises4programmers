d68eda9cb8e8b813af9585d026a6a1583f4843c3
Takeshi Suzuki <tockrock@gmail.com>
2021-05-09 09:46:40 +0900

    Copy and pasting the basic implementation

--- Ch1/CalcTipCLI/Sources/CalcTipCLI/main.swift
+++ Ch1/CalcTipCLI/Sources/CalcTipCLI/main.swift
@@ -1,1 +1,17 @@
-print("Hello, world! Again")
+func main() {
+    print("What is the amount?")
+    let priceInput = readLine() ?? ""
+    print("What is the percentage for the tip?")
+    let tipInput = readLine() ?? ""
+
+    let price = Double(priceInput)
+    let tip = Double(tipInput)
+
+    let tipAmount = price! * tip! * 0.01
+    let total = price! + tipAmount
+
+    print("tip is \(tipAmount)")
+    print("total  is \(total)")
+}
+
+main()

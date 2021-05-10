4ccf49c216a2dfc959572947d0effc0481671fe0
Takeshi Suzuki <tockrock@gmail.com>
2021-05-10 16:38:25 +0900

    Implement the example ArgumentParser project

--- Ch1/CalcTipCLI/Sources/CalcTipCLI/main.swift
+++ Ch1/CalcTipCLI/Sources/CalcTipCLI/main.swift
@@ -1,3 +1,5 @@
+import ArgumentParser
+
 func main() {
     print("What is the amount?")
     let priceInput = readLine() ?? ""
@@ -14,4 +16,30 @@ func main() {
     print("total  is \(total)")
 }
 
-main()
+// main()
+
+struct TipCalc: ParsableCommand {
+    @Flag(help: "Include a counter with each repetition.")
+    var includeCounter = false
+    
+    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
+    var count: Int?
+    
+    @Argument(help: "The phrase to repeat.")
+    var phrase: String
+    
+    mutating func run() throws {
+        let repeatCount = count ?? 10
+        
+        for i in 1...repeatCount {
+            var prefix = ""
+            if includeCounter {
+                prefix += "\(i): "
+            }
+            print("\(prefix)\(phrase)")
+        }
+    }
+    
+}
+
+TipCalc.main()

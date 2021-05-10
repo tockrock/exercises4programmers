6d501049ce42d1fef95e740cc1997770ba8b156d
Takeshi Suzuki <tockrock@gmail.com>
2021-05-10 16:14:13 +0900

    Import the ArgumentParser correctly

--- Ch1/CalcTipCLI/Package.swift
+++ Ch1/CalcTipCLI/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CalcTipCLI",
-            dependencies: ["ArgumentParser"]),
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
             name: "CalcTipCLITests",
             dependencies: ["CalcTipCLI"]),

0181a4deae3977acd1043282e00161959a3b7fd0
Takeshi Suzuki <tockrock@gmail.com>
2021-05-12 09:43:26 +0900

    multiple experiments going on at once...

--- Ch1/CalcTipCLI/Sources/CalcTipCLI/main.swift
+++ Ch1/CalcTipCLI/Sources/CalcTipCLI/main.swift
@@ -18,67 +18,50 @@ func main() {
 
 // main()
 
-func getInputWithPrompt(_ input: String) throws -> Double {
-    print("Enter \(input)", terminator: ": ")
+func getInputWithPrompt(_ prompt: String) throws -> Double {
+    print("Enter \(prompt)", terminator: ": ")
     let inputAmount = readLine() ?? ""
     
     guard let amount = Double(inputAmount) else {
-        print("Please neter a valid number for \(input)")
-        throw NonValidNumberError()
+        throw NonValidNumberError(reason: "\(inputAmount) cannot be converted to double")
     }
     return amount
 
 }
 
 struct NonValidNumberError: Error {
+    let reason: String
 }
 
 extension NonValidNumberError: CustomDebugStringConvertible {
     var debugDescription: String {
-        return "Input is a non-valid number"
+        return "Non Valid Number Error - \(reason)"
     }
 }
 
 struct TipCalc: ParsableCommand {
-//    @Flag(help: "Include a counter with each repetition.")
-//    var includeCounter = false
-//
-//    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
-//    var count: Int?
-//
-//    @Argument(help: "The phrase to repeat.")
-//    var phrase: String
     
     @Option(name: .shortAndLong, help: "The charged price.")
-    var charged: Double?
+    var billAmount: String?
     
     @Option(name: .shortAndLong, help: "The tip rate.")
-    var tip: Int?
+    var tip: String?
     
     mutating func run() throws {
 //        while charged == nil {
 //            let chargeInput = getInputWithPrompt("bill amount")
 //        }
         
-        guard let amount = try? getInputWithPrompt("bill amount") else {
+        do {
+            let amount = try getInputWithPrompt("bill amount")
+            print(amount)
+        }
+        catch {
+            print("Error: \(error)")
             return
         }
-        print(amount)
         
     }
-        
-    
-//    mutating func run() throws {
-//        let repeatCount = count ?? 10
-//
-//        for i in 1...repeatCount {
-//            var prefix = ""
-//            if includeCounter {
-//                prefix += "\(i): "
-//            }
-//            print("\(prefix)\(phrase)")
-//        }
-//    }
     
 }
 

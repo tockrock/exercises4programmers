84d0cbb2d94217644253c57abdf14c80a8b72940
Takeshi Suzuki <tockrock@gmail.com>
2021-05-16 07:29:33 +0900

    Start SwiftUI Project
    This is for the Mac only ATM

---
+++ Ch1/TipCalc/TipCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalc
+//
+//  Created by 鈴木剛 on 2021/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ch1/TipCalc/TipCalc/TipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcApp.swift
+//  TipCalc
+//
+//  Created by 鈴木剛 on 2021/05/16.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

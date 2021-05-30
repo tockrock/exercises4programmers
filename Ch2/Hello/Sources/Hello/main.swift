9669ca5c01cd7a9b0a0f61d010495edf004037fe
Takeshi Suzuki <tockrock@gmail.com>
2021-05-30 11:50:57 +0900

    Accept inputs

--- Ch2/Hello/Sources/Hello/main.swift
+++ Ch2/Hello/Sources/Hello/main.swift
@@ -1,3 +1,18 @@
 import ArgumentParser
 
-print("Hello, world!")
+
+struct Hello: ParsableCommand {
+    mutating func run() throws {
+        print("What is your name?", terminator: " ")
+        var name =  readLine() ?? "World"
+        
+        if name == "" {
+            name = "World"
+        }
+        
+        
+        print("Hello \(name), nice to meet you!")
+    }
+}
+
+Hello.main()

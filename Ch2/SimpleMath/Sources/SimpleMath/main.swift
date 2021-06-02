9243b6bdb407d3df22551f9344fc62b148192211
Takeshi Suzuki <tockrock@gmail.com>
2021-06-02 11:46:45 +0900

    Implement the basic functionality

--- Ch2/SimpleMath/Sources/SimpleMath/main.swift
+++ Ch2/SimpleMath/Sources/SimpleMath/main.swift
@@ -1,1 +1,39 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct SimpleMath {
+}
+
+extension SimpleMath: ParsableCommand {
+    func run() throws {
+        let first = askNumber("first")
+        let second = askNumber("second")
+        
+        print("""
+\(first) + \(second) = \(first + second)
+\(first) - \(second) = \(first - second)
+\(first) * \(second) = \(first * second)
+\(first) / \(second) = \(first / second)
+""")
+    }
+}
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askNumber(_ name: String) -> Double {
+    var number: Double? = nil
+    while number == nil {
+        let input = ask("What is the \(name) number?")
+        
+        if let numerical = Double(input) {
+            number = numerical
+        } else {
+            print("Expecting a number.", terminator: " ")
+        }
+    }
+    return number!
+}
+
+SimpleMath.main()

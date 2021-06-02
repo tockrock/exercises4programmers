53136744e6f1f0e1c3edcdc41681edc916fffa2b
Takeshi Suzuki <tockrock@gmail.com>
2021-06-02 09:18:38 +0900

    Feedback when nothing is entered

--- Ch2/NumbOfChars/Sources/NumbOfChars/main.swift
+++ Ch2/NumbOfChars/Sources/NumbOfChars/main.swift
@@ -5,7 +5,14 @@ struct CharacterCounter {}
 extension CharacterCounter: ParsableCommand {
     
     mutating func run() throws {
-        print("Hello World!")
+        print("What is the input string?:", terminator: " ")
+        let input = readLine() ?? ""
+        
+        if input.count > 0 {
+            print("\(input) has \(input.count) characters.")
+        } else {
+            print("You must enter a something for the word count")
+        }
     }
     
 }

8c9c27c23eb2cecfb33adb55d0ed911a3c833a58
Takeshi Suzuki <tockrock@gmail.com>
2021-06-02 09:25:27 +0900

    Create the iOS version of Number of Characters

--- Ch2/Number Counter/Number Counter/ContentView.swift
+++ Ch2/Number Counter/Number Counter/ContentView.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView {
+    @State var target_string: String = ""
+}
+
+extension ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Enter string", text: $target_string)
+            
+            if target_string.count > 0 {
+                Text("\(target_string) is \(target_string.count) Characters")
+            } else {
+                Text("Please Enter Text")
+            }
+
+        }
     }
 }
 

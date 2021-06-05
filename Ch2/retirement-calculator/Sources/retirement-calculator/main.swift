58b16737f9cc38ddb6f9df08e115de0fc98c1f8a
Takeshi Suzuki <tockrock@gmail.com>
2021-06-05 09:33:58 +0900

    Basic functionality with inputs

--- Ch2/retirement-calculator/Sources/retirement-calculator/main.swift
+++ Ch2/retirement-calculator/Sources/retirement-calculator/main.swift
@@ -8,7 +8,23 @@ func ask(_ question: String, fallback: String = "") -> String {
 struct retirementCalculator: ParsableCommand {
     
     func run() throws {
-        print("New World!")
+        let age_input = ask("What is your current age?")
+        let retirement_input = ask("At what age would you like to retire?")
+        
+        guard let age: Int = Int(age_input) else {
+            print("sorry didn't understand your age")
+            return
+        }
+        
+        guard let retirement: Int = Int(retirement_input) else {
+            print("sorry didn't understand your retirement age")
+            return
+        }
+        
+        let years = retirement - age
+        
+        print("You have \(years) years left until you can retire")
+        print("It's 2021, so you can retire in \(2021 + years).")
     }
     
 }

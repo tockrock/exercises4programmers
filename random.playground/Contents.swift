528f5bbd70eb9a8c798aae695ef929340c6c73d3
Takeshi Suzuki <tockrock@gmail.com>
2021-06-06 08:10:15 +0900

    Testing in playground

--- random.playground/Contents.swift
+++ random.playground/Contents.swift
@@ -1,1 +1,7 @@
-0.1 + 0.2
+import Foundation
+
+let length = Measurement(value: 15, unit: UnitLength.feet)
+let width = Measurement(value: 20, unit: UnitLength.feet)
+
+let area = Measurement(value: length.value * width.value, unit: UnitArea.squareFeet)
+area.converted(to: .squareMeters)

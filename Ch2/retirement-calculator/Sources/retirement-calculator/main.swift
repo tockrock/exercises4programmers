c2395ad2093409c0a4338eda303c755904e99be2
Takeshi Suzuki <tockrock@gmail.com>
2021-06-05 09:48:06 +0900

    Better validation

--- Ch2/retirement-calculator/Sources/retirement-calculator/main.swift
+++ Ch2/retirement-calculator/Sources/retirement-calculator/main.swift
@@ -5,26 +5,37 @@ func ask(_ question: String, fallback: String = "") -> String {
     return readLine() ?? fallback
 }
 
+func intFromInput(_ question: String) -> Int {
+    
+    while true {
+        let input = ask(question)
+        
+        guard let number = Int(input) else {
+            print("Not a number.", terminator: " ")
+            continue
+        }
+        
+        guard number > 0 else {
+            print("Enter a positive number.", terminator: " ")
+            continue
+        }
+        
+        return number
+    }
+}
+
 struct retirementCalculator: ParsableCommand {
     
     func run() throws {
-        let age_input = ask("What is your current age?")
-        let retirement_input = ask("At what age would you like to retire?")
-        
-        guard let age: Int = Int(age_input) else {
-            print("sorry didn't understand your age")
-            return
-        }
-        
-        guard let retirement: Int = Int(retirement_input) else {
-            print("sorry didn't understand your retirement age")
-            return
-        }
-        
+        let age = intFromInput("What is your current age?")
+        let retirement = intFromInput("At what age would you like to retire?")
+
         let years = retirement - age
         
+        let current_year = 2022
+        
         print("You have \(years) years left until you can retire")
-        print("It's 2021, so you can retire in \(2021 + years).")
+        print("It's \(current_year), so you can retire in \(current_year + years).")
     }
     
 }

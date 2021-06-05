e686218ee4eaa2ea50e49c26089c19209f3e8baa
Takeshi Suzuki <tockrock@gmail.com>
2021-06-05 09:28:34 +0900

    Basic underpinning for the command

--- Ch2/retirement-calculator/Sources/retirement-calculator/main.swift
+++ Ch2/retirement-calculator/Sources/retirement-calculator/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import ArgumentParser
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+struct retirementCalculator: ParsableCommand {
+    
+    func run() throws {
+        print("New World!")
+    }
+    
+}
+
+retirementCalculator.main()

084ac8e8448c5522bd6192403f37b9200ffa3dc5
Takeshi Suzuki <tockrock@gmail.com>
2021-06-06 08:10:44 +0900

    Initial implementation using Measurement library

--- 7/room-area/Sources/room-area/main.swift
+++ 7/room-area/Sources/room-area/main.swift
@@ -1,8 +1,51 @@
+import Foundation
 import ArgumentParser
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askForInt(_ question: String) -> Int {
+    while true {
+        let input = ask(question)
+        
+        guard let number: Int = Int(input) else {
+            print("Please enter a number.", terminator: " ")
+            continue
+        }
+        
+        guard number > 0 else {
+            print("Please enter a positive number", terminator: " " )
+            continue
+        }
+        
+        return number
+    }
+}
 
 struct roomArea : ParsableCommand {
     func run() throws {
-        print("Hello World")
+        let length = askForInt("What is the length of the room in feet?")
+        let width = askForInt("What is the width of the room in feet?")
+        let area = length * width
+
+        
+        print("You entered dimensions of \(length) feet by \(width) feet.")
+        print("The area is:")
+        
+        if #available(macOS 10.12, *) {
+            let unitArea = Measurement(value: Double(area), unit: UnitArea.squareFeet)
+            let converted = unitArea.converted(to: .squareMeters)
+            
+            print(unitArea)
+            print(converted)
+            
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        
     }
 }
 

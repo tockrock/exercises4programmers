425dfd85c1dc9f8ea2fed900f04376b8f897fa1c
Takeshi Suzuki <tockrock@gmail.com>
2021-06-06 10:38:19 +0900

    Add support for input

---
+++ 7/AreaCalculator/AreaCalculator/InputView.swift
@@ -0,0 +1,20 @@
+//
+//  InputView.swift
+//  AreaCalculator
+//
+//  Created by 鈴木剛 on 2021/06/06.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
---
+++ 7/AreaCalculator/AreaCalculator/Unit.swift
@@ -0,0 +1,76 @@
+//
+//  Unit.swift
+//  AreaCalculator
+//
+//  Created by 鈴木剛 on 2021/06/06.
+//
+
+import Foundation
+import SwiftUI
+
+enum Unit {
+    case feet
+    case meter
+}
+
+enum State {
+    case empty
+    case nonNumerical
+    case notPositive
+    case correct
+    
+    var color: Color {
+        switch self {
+        case .empty, .correct:
+            return .clear
+        case .nonNumerical, .notPositive:
+            return .accentColor
+        }
+    }
+    
+    var warning: String {
+        switch self {
+        case .empty:
+            return "Please enter a value"
+        case .nonNumerical:
+            return "Enter a number"
+        case .notPositive:
+            return "Enter a positive number"
+        case .correct:
+            return "Valid Input"
+        }
+    }
+        
+}
+
+
+class inputHandler: ObservableObject {
+    var input: String = "" {
+        didSet {
+            validate()
+        }
+    }
+    var value: Double = 0.0
+    
+    @Published var state: State = .empty
+    
+    func validate() {
+        guard input.count > 0 else {
+            state = .empty
+            return
+        }
+        
+        guard let input: Double = Double(input) else {
+            state = .nonNumerical
+            return
+        }
+        
+        guard input > 0 else {
+            state = .notPositive
+            return
+        }
+        
+        value = input
+        state = .correct
+    }
+}

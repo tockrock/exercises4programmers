27cc40a82827c10a28467836ec1ff3386eb696a0
Takeshi Suzuki <tockrock@gmail.com>
2021-06-06 09:16:09 +0900

    Experiments with doing custom operations

--- random.playground/Contents.swift
+++ random.playground/Contents.swift
@@ -1,7 +1,40 @@
 import Foundation
 
 let length = Measurement(value: 15, unit: UnitLength.feet)
-let width = Measurement(value: 20, unit: UnitLength.feet)
+let width = Measurement(value: 20, unit: UnitLength.meters)
 
 let area = Measurement(value: length.value * width.value, unit: UnitArea.squareFeet)
 area.converted(to: .squareMeters)
+
+enum specifiedUnit {
+    case feet
+    case meter
+}
+
+let unit = specifiedUnit.feet
+
+length.unit == UnitLength.feet
+
+infix operator *
+
+extension Measurement {
+    
+    static func * (lhs: Measurement<UnitLength>, rhs: Measurement<UnitLength>) -> Double {
+        let area = lhs.value * rhs.value
+        return area
+        
+//        switch lhs.unit {
+//        case .feet:
+//            return Measurement(value: area, unit: UnitArea.squareFeet)
+//        default:
+//            return Measurement(value: area, unit: UnitArea.squareMeters)
+//        }
+//
+    }
+    
+}
+
+
+length + width
+length * width
+

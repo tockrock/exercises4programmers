7b8447c71aa3bda6dca3a18313a4769bb4e04820
Takeshi Suzuki <tockrock@gmail.com>
2021-06-06 10:01:26 +0900

    Start new iPhone app

---
+++ 7/AreaCalculator/AreaCalculator/AreaCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaCalculatorApp.swift
+//  AreaCalculator
+//
+//  Created by 鈴木剛 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct AreaCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 7/AreaCalculator/AreaCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AreaCalculator
+//
+//  Created by 鈴木剛 on 2021/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

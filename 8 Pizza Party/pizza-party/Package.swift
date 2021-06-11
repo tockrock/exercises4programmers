286ad939e261b931a7f88ad335f47e633c16eb58
Takeshi Suzuki <tockrock@gmail.com>
2021-06-11 11:17:49 +0900

    Attempt for localizations

--- 8 Pizza Party/pizza-party/Package.swift
+++ 8 Pizza Party/pizza-party/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "pizza-party",
+    defaultLocalization: "en",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),

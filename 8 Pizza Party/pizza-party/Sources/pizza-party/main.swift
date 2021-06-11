5cc5fdfda4b48b29c448558c5cb866fbc94ef054
Takeshi Suzuki <tockrock@gmail.com>
2021-06-11 09:18:12 +0900

    Basic structure to ask for input

--- 8 Pizza Party/pizza-party/Sources/pizza-party/main.swift
+++ 8 Pizza Party/pizza-party/Sources/pizza-party/main.swift
@@ -1,10 +1,25 @@
 import ArgumentParser
 
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askInt(_ question: String) -> Int {
+    while true {
+        var input = ask(question)
+        
+        
+        return 1
+    }
+}
 
 struct pizzaParty: ParsableCommand {
     
     func run() throws {
-        print("hello world")
+        let people_count = askInt("How many people?")
+        
+        print("\(people_count) people")
     }
     
 }

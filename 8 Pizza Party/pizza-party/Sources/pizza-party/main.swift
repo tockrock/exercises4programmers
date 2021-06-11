0b579fc3a5dfbdddf491c4c9734e7adc3cca59c7
Takeshi Suzuki <tockrock@gmail.com>
2021-06-11 11:18:46 +0900

    Pluralization

--- 8 Pizza Party/pizza-party/Sources/pizza-party/main.swift
+++ 8 Pizza Party/pizza-party/Sources/pizza-party/main.swift
@@ -23,17 +23,17 @@ func askInt(_ question: String) -> Int {
     }
 }
 
-func divide(pizza pizza_count: Int, with people_count: Int) -> (Int, Int) {
-    let slices = pizza_count * 8
-    
-    let each: Int = slices / people_count // just in case
-    let remain = slices % people_count
-    
-    
-    return (each, remain)
-}
 
 struct pizzaParty: ParsableCommand {
+    func divide(pizza pizza_count: Int, with people_count: Int) -> (Int, Int) {
+        let slices = pizza_count * 8
+        
+        let each: Int = slices / people_count // just in case
+        let remain = slices % people_count
+        
+        
+        return (each, remain)
+    }
     
     func run() throws {
         let people_count = askInt("How many people?")
@@ -41,9 +41,30 @@ struct pizzaParty: ParsableCommand {
         
         let (each, remain) = divide(pizza: pizza_count, with: people_count)
         
-        print("\(people_count) people with \(pizza_count) pizzas")
-        print("Each person gets \(each) pieces of pizza")
-        print("There are \(remain) leftover pieces.")
+        switch pizza_count {
+        case 1:
+            print("\(people_count) people with \(pizza_count) pizzas")
+        default:
+            print("\(people_count) people with \(pizza_count) pizza")
+        }
+        
+        switch each {
+        case 0:
+            print("No one gets a piece of pizza :(")
+        case 1:
+            print("Each person gets \(each) piece of pizza")
+        default:
+            print("Each person gets \(each) pieces of pizza")
+        }
+        
+        switch remain {
+        case 0:
+            print("No left overs!")
+        case 1:
+            print("There is \(remain) piece leftover.")
+        default:
+            print("There are \(remain) pieces leftover.")
+        }
     }
     
 }

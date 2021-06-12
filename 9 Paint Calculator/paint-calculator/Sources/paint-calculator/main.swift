27e8274f712c0d4c93da900b91318a6a2b68352b
Takeshi Suzuki <tockrock@gmail.com>
2021-06-13 08:59:52 +0900

    Asking for ints

--- 9 Paint Calculator/paint-calculator/Sources/paint-calculator/main.swift
+++ 9 Paint Calculator/paint-calculator/Sources/paint-calculator/main.swift
@@ -1,8 +1,33 @@
 import ArgumentParser
 
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askForInt(_ question: String) -> Int {
+    while true {
+        let input = ask(question)
+        
+        guard let input: Int = Int(input) else {
+            print("Please enter a number.", terminator: " ")
+            continue
+        }
+        
+        guard input > 0 else {
+            print("Please enter a positive number", terminator: " ")
+            continue
+        }
+        
+        return input
+    }
+}
+
 struct PaintCalculator: ParsableCommand {
     func run() throws {
-        print("Hello World!")
+        let sqft = askForInt("How many suare feet of paint do you need?")
+        
+        print("You need to paint \(sqft) square feet.")
     }
 }
 

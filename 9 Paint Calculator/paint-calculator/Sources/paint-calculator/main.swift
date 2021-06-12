d443626cccfdd1a6bcdbe7187de8729cff176459
Takeshi Suzuki <tockrock@gmail.com>
2021-06-13 08:52:37 +0900

    Hello World

--- 9 Paint Calculator/paint-calculator/Sources/paint-calculator/main.swift
+++ 9 Paint Calculator/paint-calculator/Sources/paint-calculator/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct PaintCalculator: ParsableCommand {
+    func run() throws {
+        print("Hello World!")
+    }
+}
+
+PaintCalculator.main()

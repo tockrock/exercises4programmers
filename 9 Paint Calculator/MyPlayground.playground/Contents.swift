e61855014bf2ad837f63b60a3052ff5c8305f98c
Takeshi Suzuki <tockrock@gmail.com>
2021-06-13 09:12:16 +0900

    Testing in Playground

---
+++ 9 Paint Calculator/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+
+360 / 350
+let area = 360
+let perGallon = 350
+area / perGallon
+ceil(360 / 350)
+ceil(Double(Double(area) / Double(perGallon)))
+Int(ceil(360 / 350))

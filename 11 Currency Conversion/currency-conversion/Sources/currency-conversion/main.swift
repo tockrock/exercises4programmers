38e9fce4bd46987a5ffe2ca69b769fe91e8cf060
Takeshi Suzuki <tockrock@gmail.com>
2021-06-22 10:12:48 +0900

    First input

--- 11 Currency Conversion/currency-conversion/Sources/currency-conversion/main.swift
+++ 11 Currency Conversion/currency-conversion/Sources/currency-conversion/main.swift
@@ -1,11 +1,44 @@
 import ArgumentParser
 
-struct CurrencyConversion: ParsableCommand {
-    func run() throws {
-        print("Hello, Currency Conversion")
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func invalidInput() {
+    print("Please enter a positive number:", terminator: " ")
+}
+
+func askForDouble(_ question: String) -> Double {
+    while true {
+        let input = ask(question)
+        
+        guard input.count > 0 else {
+            invalidInput()
+            continue
+        }
+        
+        guard let input: Double = Double(input) else {
+            invalidInput()
+            continue
+        }
+        
+        guard input > 0 else {
+            invalidInput()
+            continue
+        }
+        
+        return input
     }
 }
 
-print("Hello, world!")
+struct CurrencyConversion: ParsableCommand {
+    func run() throws {
+        let amountFrom = askForDouble("How many euros are you exchanging?")
+        
+        
+        print("\(amountFrom) euros at an exchange rate of is U.S. dollars.")
+    }
+}
 
 CurrencyConversion.main()

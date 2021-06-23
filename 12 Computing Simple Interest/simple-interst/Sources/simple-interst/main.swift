1fe8fa40ec59478196f89b84890ea75d097d0cfb
Takeshi Suzuki <tockrock@gmail.com>
2021-06-23 10:16:58 +0900

    Hello World

--- 12 Computing Simple Interest/simple-interst/Sources/simple-interst/main.swift
+++ 12 Computing Simple Interest/simple-interst/Sources/simple-interst/main.swift
@@ -1,1 +1,12 @@
+import ArgumentParser
+
+
+struct SimpleInterest: ParsableCommand {
+    func run() throws {
+        print("Hello Simple Interst")
+    }
+}
+
 print("Hello, world!")
+
+SimpleInterest.main()

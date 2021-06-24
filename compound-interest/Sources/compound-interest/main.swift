d53be85e39de5647eece976c70de602e2521552b
Takeshi Suzuki <tockrock@gmail.com>
2021-06-24 09:50:25 +0900

    First input

--- compound-interest/Sources/compound-interest/main.swift
+++ compound-interest/Sources/compound-interest/main.swift
@@ -1,8 +1,37 @@
 import ArgumentParser
 
+func notPositive() {
+    print("Not a positive number.", terminator: " ")
+
+}
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askForDouble(_ question: String) -> Double {
+    while true {
+        let input = ask(question)
+        
+        guard let input: Double = Double(input) else {
+            notPositive()
+            continue
+        }
+        
+        guard input > 0 else {
+            notPositive()
+            continue
+        }
+        
+        return input
+    }
+}
+
 struct compoundInterest: ParsableCommand {
     func run() throws {
-        print("Hello Compound Interest")
+        let principal = askForDouble("What is the principal amount?")
+        print("The principal amount is \(principal)")
     }
 }
 

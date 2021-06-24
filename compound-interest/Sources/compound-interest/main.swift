1e746e6a9c5614fb1807f9a9c02a7659baaed9ac
Takeshi Suzuki <tockrock@gmail.com>
2021-06-24 10:40:10 +0900

    Calculation and output

--- compound-interest/Sources/compound-interest/main.swift
+++ compound-interest/Sources/compound-interest/main.swift
@@ -33,14 +33,44 @@ func askForRoundedInt(_ question: String) -> Int {
     return Int(ceil(askForDouble(question)))
 }
 
+func calculateCompoundInterest(principal: Double, rate: Double, years: Int, compoundPerYear: Int) -> Double {
+    let doubleYears = Double(years)
+    let doubleCompounds = Double(compoundPerYear)
+    
+    let amount = principal * pow((1 + rate/doubleCompounds),doubleCompounds * doubleYears)
+    
+    return amount
+}
+
+enum outputOption {
+    case currency
+    case percent
+}
+
+func output(_ amount: Double, as option: outputOption) -> String {
+    let formatter = NumberFormatter()
+    switch option {
+    case .currency:
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "USD"
+    case .percent:
+        formatter.numberStyle = .percent
+        formatter.maximumFractionDigits = 2
+    }
+    return formatter.string(from: NSNumber(value: amount))!
+}
+
 struct compoundInterest: ParsableCommand {
     func run() throws {
         let principal = askForDouble("What is the principal amount?")
-        let bareRate = askForDouble("What is the rate?") / 100
+        let rate = askForDouble("What is the rate?") / 100
         let years = askForRoundedInt("What is the number of years?")
         let compoundPerYear = askForRoundedInt("What is the number of times the interest is compounded per year?")
         
-        print("\(principal) invested at \(bareRate) for \(years) compounded \(compoundPerYear) times per year is \\(amount)")
+        let amount = calculateCompoundInterest(principal: principal, rate: rate, years: years, compoundPerYear: compoundPerYear)
+        
+        
+        print("\(output(principal, as: .currency)) invested at \(output(rate, as: .percent)) for \(years) compounded \(compoundPerYear) times per year is \(output(amount, as: .currency))")
     }
 }
 

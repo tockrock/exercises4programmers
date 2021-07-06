cc31f09debbbd0d4fc4da7df7ebf123a91e9a145
Takeshi Suzuki <tockrock@gmail.com>
2021-07-07 07:39:36 +0900

    Hello World

--- 14 Tax Calculator/tax-calculator/Sources/tax-calculator/main.swift
+++ 14 Tax Calculator/tax-calculator/Sources/tax-calculator/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct taxCalculator: ParsableCommand {
+    func run() throws {
+        print("Hello Tax Calculator!")
+    }
+}
+
+taxCalculator.main()

c2b99fb6db5a9904e00391c93729d184c208f4b1
Takeshi Suzuki <tockrock@gmail.com>
2021-07-07 08:31:18 +0900

    Hello World

--- 15 Passworld Validation/password-validation/Sources/password-validation/main.swift
+++ 15 Passworld Validation/password-validation/Sources/password-validation/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct passwordValidator: ParsableCommand {
+    func run() throws {
+        print("Hello Password Validator!")
+    }
+}
+
+passwordValidator.main()

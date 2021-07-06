a7da054a06b105811c6578c9984f38865c909d89
Takeshi Suzuki <tockrock@gmail.com>
2021-07-07 08:03:25 +0900

    basic implementation

--- 14 Tax Calculator/tax-calculator/Sources/tax-calculator/main.swift
+++ 14 Tax Calculator/tax-calculator/Sources/tax-calculator/main.swift
@@ -1,8 +1,52 @@
 import ArgumentParser
 
+func notPositiveNumber() {
+    print("Not a positive number.", terminator: " ")
+}
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askDouble(_ question: String) -> Double {
+    while true {
+        let input = ask(question)
+        
+        guard let input: Double = Double(input) else {
+            notPositiveNumber()
+            continue
+        }
+        
+        guard input > 0 else {
+            notPositiveNumber()
+            continue
+        }
+        
+        return input
+    }
+}
+
 struct taxCalculator: ParsableCommand {
     func run() throws {
-        print("Hello Tax Calculator!")
+        let order = askDouble("What is the order amount?")
+        let subtotal = order
+        
+        
+        var tax = 0.0
+        
+        let state = ask("What is the state").lowercased()
+        
+        if state == "wi" {
+            
+            tax = subtotal * 0.055
+            print("The Subtotal is \(subtotal)")
+            print("The tax is \(tax)")
+        }
+        
+        let total = subtotal + tax
+        
+        print("The total is \(total)")
     }
 }
 

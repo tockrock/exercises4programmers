0f9d9fda7567e533c9112a98382e9d7e5289baf9
Takeshi Suzuki <tockrock@gmail.com>
2021-07-07 08:47:36 +0900

    Basic implementation

--- 15 Passworld Validation/password-validation/Sources/password-validation/main.swift
+++ 15 Passworld Validation/password-validation/Sources/password-validation/main.swift
@@ -1,8 +1,21 @@
 import ArgumentParser
 
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
 struct passwordValidator: ParsableCommand {
     func run() throws {
-        print("Hello Password Validator!")
+        let username = ask("What is the username")
+        let password = ask("What is the password")
+        
+        if username == "tockrock" && password == "foo" {
+            print("Hello \(username)!")
+        } else {
+            print("Unrecognized username/password")
+        }
+        
     }
 }
 

06c58d1f0e7f0252b804a0c04ff4c7e59a4a712c
Takeshi Suzuki <tockrock@gmail.com>
2021-07-07 09:03:41 +0900

    Input

--- 16 Legal Driving Age/legal-driving-age/Sources/legal-driving-age/main.swift
+++ 16 Legal Driving Age/legal-driving-age/Sources/legal-driving-age/main.swift
@@ -1,8 +1,33 @@
 import ArgumentParser
 
+let drivingAge = 16
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askInt(_ question: String) -> Int {
+    while true {
+        let input = ask(question)
+        
+        guard let input: Int = Int(input) else {
+            continue
+        }
+        
+        guard input > 0 else {
+            continue
+        }
+        
+        return input
+    }
+}
+
 struct legalDrivingAge: ParsableCommand {
     func run() throws {
-        print("Hello Legal Driving Age")
+        let age = askInt("What is your age?")
+        
+        print("Your age is \(age)")
     }
 }
 

acd9f840bfb116bcee15ed6392d358c96e8ba6fb
Takeshi Suzuki <tockrock@gmail.com>
2021-07-07 09:14:43 +0900

    Hello world!

--- 17 Blood Alcohol Calculator/blood-alcohol-calculator/Sources/blood-alcohol-calculator/main.swift
+++ 17 Blood Alcohol Calculator/blood-alcohol-calculator/Sources/blood-alcohol-calculator/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct bloodAlcoholCalculator: ParsableCommand {
+    func run() throws {
+        print("Hello Blood Alcohol Calculator!")
+    }
+}
+
+bloodAlcoholCalculator.main()

97f40f8d05eb01b828fdeb1fd874f8d30b0446cd
Takeshi Suzuki <tockrock@gmail.com>
2021-07-08 10:43:58 +0900

    Create a SwiftUI App

---
+++ 18 Temperature Converter/Temperature Converter/Temperature Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Temperature Converter
+//
+//  Created by 鈴木剛 on 2021/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 18 Temperature Converter/Temperature Converter/Temperature Converter/Temperature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConverterApp.swift
+//  Temperature Converter
+//
+//  Created by 鈴木剛 on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

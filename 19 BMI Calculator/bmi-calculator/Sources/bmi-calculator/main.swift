d3c37f8c4e361ad71f08d959daf5034cdf0ccd58
Takeshi Suzuki <tockrock@gmail.com>
2021-07-08 16:01:30 +0900

    Implement the basics with the

--- 19 BMI Calculator/bmi-calculator/Sources/bmi-calculator/main.swift
+++ 19 BMI Calculator/bmi-calculator/Sources/bmi-calculator/main.swift
@@ -1,8 +1,34 @@
+import Foundation
 import ArgumentParser
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askDouble(_ question: String) -> Double {
+    while true {
+        let input = ask(question)
+        
+        guard let input: Double = Double(input) else {
+            continue
+        }
+        
+        guard input > 0 else {
+            continue
+        }
+        
+        return input
+    }
+}
 
 struct bmiCalculator: ParsableCommand {
     func run() throws {
-        print("Hello BMI Calculator")
+        let weight = askDouble("What is your weight? (kg)")
+        let height = askDouble("What is your height? (cm)")
+        
+        let bmi = weight / pow(height/100, 2)
+        print("Your BMI is \(bmi)")
     }
 }
 

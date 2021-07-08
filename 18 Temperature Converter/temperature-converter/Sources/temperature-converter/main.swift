7a723d5019765a56ea46f548f7a2bdd83df840cf
Takeshi Suzuki <tockrock@gmail.com>
2021-07-08 09:47:21 +0900

    Selecting the Conversion Method

--- 18 Temperature Converter/temperature-converter/Sources/temperature-converter/main.swift
+++ 18 Temperature Converter/temperature-converter/Sources/temperature-converter/main.swift
@@ -1,8 +1,38 @@
 import ArgumentParser
 
+enum ConversionMethod {
+    case c
+    case f
+}
+
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askConversionMethod() -> ConversionMethod {
+    while true {
+        print("Press C to convert from Fahrenheit to Celsius.",
+              "Press F to convert from Celsius to Fahrenheit.", separator: "\n")
+        
+        let input = ask("Your choice")
+        
+        switch input.lowercased() {
+        case "c":
+            return ConversionMethod.c
+        case "f":
+            return ConversionMethod.f
+        default:
+            continue
+        }
+    }
+}
+
 struct temperatureConverter: ParsableCommand {
     func run() throws {
-        print("Hello Temperature Converter!")
+        let conversionMethod = askConversionMethod()
+        
+        print("You've selected \(conversionMethod)")
     }
 }
 

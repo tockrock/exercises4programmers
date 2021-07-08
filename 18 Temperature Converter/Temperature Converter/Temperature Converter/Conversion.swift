3d55f7ca6eeac55ad97d187e220fd9d6197508d5
Takeshi Suzuki <tockrock@gmail.com>
2021-07-08 10:45:16 +0900

    Create the file to put the conversion view

---
+++ 18 Temperature Converter/Temperature Converter/Temperature Converter/Conversion.swift
@@ -0,0 +1,8 @@
+//
+//  Conversion.swift
+//  Temperature Converter
+//
+//  Created by 鈴木剛 on 2021/07/08.
+//
+
+import Foundation
---
+++ 18 Temperature Converter/Temperature Converter/Temperature Converter/ConversionView.swift
@@ -0,0 +1,20 @@
+//
+//  ConversionView.swift
+//  Temperature Converter
+//
+//  Created by 鈴木剛 on 2021/07/08.
+//
+
+import SwiftUI
+
+struct ConversionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConversionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversionView()
+    }
+}

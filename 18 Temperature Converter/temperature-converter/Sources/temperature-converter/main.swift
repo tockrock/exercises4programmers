defaf361b39dd061c7c26254f614b350cf4390f6
Takeshi Suzuki <tockrock@gmail.com>
2021-07-08 09:39:55 +0900

    Hello World!

--- 18 Temperature Converter/temperature-converter/Sources/temperature-converter/main.swift
+++ 18 Temperature Converter/temperature-converter/Sources/temperature-converter/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct temperatureConverter: ParsableCommand {
+    func run() throws {
+        print("Hello Temperature Converter!")
+    }
+}
+
+temperatureConverter.main()

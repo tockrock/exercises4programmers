dfc18fa54058fb050c5203ed94081ed7cffd6e9d
Takeshi Suzuki <tockrock@gmail.com>
2021-07-16 08:39:58 +0900

    Create a GUI app

---
+++ 19 BMI Calculator/BMI Calculator/BMI Calculator/BMI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI Calculator
+//
+//  Created by 鈴木剛 on 2021/07/16.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

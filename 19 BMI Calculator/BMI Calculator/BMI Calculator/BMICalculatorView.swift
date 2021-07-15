bd82d5196bd84c4c3575481eba24fac6bf6268a2
Takeshi Suzuki <tockrock@gmail.com>
2021-07-16 08:59:54 +0900

    Create new View for the BMI Calculator

---
+++ 19 BMI Calculator/BMI Calculator/BMI Calculator/BMICalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  BMICalculatorView.swift
+//  BMI Calculator
+//
+//  Created by 鈴木剛 on 2021/07/16.
+//
+
+import SwiftUI
+
+struct BMICalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BMICalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        BMICalculatorView()
+    }
+}

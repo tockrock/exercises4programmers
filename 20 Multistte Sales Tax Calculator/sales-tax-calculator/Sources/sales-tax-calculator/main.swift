e5da3268929bbe362a4c9d963ff5a7d45f840e35
Takeshi Suzuki <tockrock@gmail.com>
2021-07-17 08:22:52 +0900

    Hello World

--- 20 Multistte Sales Tax Calculator/sales-tax-calculator/Sources/sales-tax-calculator/main.swift
+++ 20 Multistte Sales Tax Calculator/sales-tax-calculator/Sources/sales-tax-calculator/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct SalesTaxCalculator: ParsableCommand {
+    func run() throws {
+        print("Hello Sales Tax Calculator!")
+    }
+}
+
+SalesTaxCalculator.main()

fd0945a81fe274f4cc160022034259e7e1f01d95
Takeshi Suzuki <tockrock@gmail.com>
2021-07-18 10:30:35 +0900

    Implement comparison

--- 22 Comparing Numbers/compare-numbers/Sources/compare-numbers/main.swift
+++ 22 Comparing Numbers/compare-numbers/Sources/compare-numbers/main.swift
@@ -16,14 +16,27 @@ func askForDouble(_ question: String) -> Double {
     }
 }
 
+func largerOf(_ first: Double, and second: Double) -> Double {
+    if first > second {
+        return first
+    }
+    return second
+}
+
 struct CompareNumbers: ParsableCommand {
     func run() throws {
         var numbers: [Double] = []
         numbers.append(askForDouble("Enter the first number"))
         numbers.append(askForDouble("Enter the second number"))
         numbers.append(askForDouble("Enter the third number"))
+
+        var max = numbers [0]
+
+        for i in 1..<numbers.count {
+            max = largerOf(max, and: numbers[i])
+        }
         
-        print("You've entered the numbers \(numbers)")
+        print("The largest number you've entered is \(max)")
     }
 }
 

6d4c382dfc106581353ba55ed0dc7dcd845258e1
Takeshi Suzuki <tockrock@gmail.com>
2021-07-18 10:24:47 +0900

    Ask for input

--- 22 Comparing Numbers/compare-numbers/Sources/compare-numbers/main.swift
+++ 22 Comparing Numbers/compare-numbers/Sources/compare-numbers/main.swift
@@ -1,8 +1,26 @@
 import ArgumentParser
 
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askForDouble(_ question: String) -> Double {
+    while true {
+        let input = ask(question)
+        guard let input: Double = Double(input) else {
+            continue
+        }
+        
+        return input
+    }
+}
+
 struct CompareNumbers: ParsableCommand {
     func run() throws {
-        print("Hello Compare Numbers")
+        let number = askForDouble("Enter the first number")
+        
+        print("You've entered the number \(number)")
     }
 }
 

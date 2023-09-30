8159cb00ff9117355f8b186150feb2110995bf6f
Takeshi Suzuki <tockrock@gmail.com>
2023-09-30 21:38:36 +0900

    something remaing from way back

--- 23 Car Issues/car-issues-helper/Sources/car-issues-helper/main.swift
+++ 23 Car Issues/car-issues-helper/Sources/car-issues-helper/main.swift
@@ -1,8 +1,37 @@
 import ArgumentParser
 
+func ask(_ question: String, fallback: String = "") -> String {
+    print(question, terminator: ": ")
+    return readLine() ?? fallback
+}
+
+func askBool(_ question: String) -> Bool {
+    while true {
+        let input = ask(question + " (yn)")
+        
+        switch input {
+        case "y":
+            return true
+        case "n":
+            return false
+        default:
+             continue
+        }
+    }
+}
+
+struct questionStruct {
+    let question = ""
+    let yes = ""
+    let no = ""
+    let answer = ""
+}
+
 struct CarIssuesHelper: ParsableCommand {
     func run() throws {
-        print("Hello Car Issues Helper")
+        
+        let first_answer = askBool("Is the car silent when you turn the key?")
+        print("Hello Car Issues Helper \(first_answer)")
     }
 }
 

4e8ac7215bead7aa11ba376cece7dfae927a8ac1
Takeshi Suzuki <tockrock@gmail.com>
2023-10-02 20:27:44 +0900

    Create a SwiftUI app

---
+++ current/1 Saying Hello/Saying Hello/Saying Hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Saying Hello
+//
+//  Created by 鈴木剛 on 2023/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ current/1 Saying Hello/Saying Hello/Saying Hello/Saying_HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saying_HelloApp.swift
+//  Saying Hello
+//
+//  Created by 鈴木剛 on 2023/10/02.
+//
+
+import SwiftUI
+
+@main
+struct Saying_HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

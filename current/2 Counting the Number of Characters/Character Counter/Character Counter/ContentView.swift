c3c278a2b7ae6b4c78bc454ed94e0404f84a7c11
Takeshi Suzuki <tockrock@gmail.com>
2023-10-03 22:25:08 +0900

    implementation

--- current/2 Counting the Number of Characters/Character Counter/Character Counter/ContentView.swift
+++ current/2 Counting the Number of Characters/Character Counter/Character Counter/ContentView.swift
@@ -6,14 +6,25 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable class InputModel {
+    var input: String = ""
+    var output: String {
+        guard !input.isEmpty else {
+            return "Please enter a text below"
+        }
+        return "\(input) has \(input.count) characters."
+    }
+}
 
 struct ContentView: View {
+    @Bindable var model: InputModel = InputModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(model.output)
+            TextField("Text", text: $model.input)
         }
         .padding()
     }

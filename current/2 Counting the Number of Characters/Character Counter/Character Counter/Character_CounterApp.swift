fe088408fb744ffdf9a9a0338ee3881f1127ac0f
Takeshi Suzuki <tockrock@gmail.com>
2023-10-03 22:20:01 +0900

    Create Project for Exercise 2

---
+++ current/2 Counting the Number of Characters/Character Counter/Character Counter/Character_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Character_CounterApp.swift
+//  Character Counter
+//
+//  Created by 鈴木剛 on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct Character_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

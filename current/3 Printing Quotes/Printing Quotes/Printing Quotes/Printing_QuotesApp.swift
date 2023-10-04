6c8d15b68fd46e2cbd564fd7877e686140d04628
Takeshi Suzuki <tockrock@gmail.com>
2023-10-04 20:04:21 +0900

    Create Project for 3 Printing Quotes

---
+++ current/3 Printing Quotes/Printing Quotes/Printing Quotes/Printing_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Printing_QuotesApp.swift
+//  Printing Quotes
+//
+//  Created by 鈴木剛 on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct Printing_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

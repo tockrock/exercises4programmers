1ff55f9cfabedfe489671a30389e1b766e72f7c3
Takeshi Suzuki <tockrock@gmail.com>
2023-10-04 20:26:12 +0900

    Create Project for 4 Mad Lib

---
+++ current/4 Mad Lib/Mad Lib/Mad Lib/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mad Lib
+//
+//  Created by 鈴木剛 on 2023/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ current/4 Mad Lib/Mad Lib/Mad Lib/Mad_LibApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mad_LibApp.swift
+//  Mad Lib
+//
+//  Created by 鈴木剛 on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct Mad_LibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

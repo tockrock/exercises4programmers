087f59b467df17cbec45d2345135db1ac4c87e0a
Takeshi Suzuki <tockrock@gmail.com>
2023-10-04 20:47:57 +0900

    Create project for exercise 5 Simple Math

---
+++ current/5 Simple Math/Simple Math/Simple Math/Simple_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_MathApp.swift
+//  Simple Math
+//
+//  Created by 鈴木剛 on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct Simple_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

74dec22fd1c1540b76f51776d669337549807720
Takeshi Suzuki <tockrock@gmail.com>
2023-10-05 20:08:50 +0900

    Create new project for exercise 6 Retirement Calculator

---
+++ current/6 Retirement Calculator/Retirement Calculator/Retirement Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Retirement Calculator
+//
+//  Created by 鈴木剛 on 2023/10/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ current/6 Retirement Calculator/Retirement Calculator/Retirement Calculator/Retirement_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retirement_CalculatorApp.swift
+//  Retirement Calculator
+//
+//  Created by 鈴木剛 on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct Retirement_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

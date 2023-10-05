09a03cc60bc95b5f91e5e5462f1a640948b48a30
Takeshi Suzuki <tockrock@gmail.com>
2023-10-05 20:07:27 +0900

    Implement exercise 5

--- current/5 Simple Math/Simple Math/Simple Math/ContentView.swift
+++ current/5 Simple Math/Simple Math/Simple Math/ContentView.swift
@@ -6,14 +6,42 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable class InputModel {
+    var firstInput = "" {
+        didSet {
+            firstNumber = Int(firstInput)
+        }
+    }
+    var secondInput = "" {
+        didSet {
+            secondNumber = Int(secondInput)
+        }
+    }
+    
+    private(set) var firstNumber: Int? = nil
+    private(set) var secondNumber: Int? = nil
+
+}
 
 struct ContentView: View {
+    @Bindable var model = InputModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("First Number", text: $model.firstInput)
+            TextField("Second Number", text: $model.secondInput)
+            
+            if let first = model.firstNumber,
+               let second = model.secondNumber {
+                Text("""
+                     \(first) + \(second) = \(first + second)
+                     \(first) - \(second) = \(first - second)
+                     \(first) * \(second) = \(first * second)
+                     \(first) / \(second) = \(Double(first) / Double(second))
+                     """)
+            }
         }
         .padding()
     }

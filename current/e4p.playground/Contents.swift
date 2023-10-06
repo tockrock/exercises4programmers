b1c6bfb4c0ce3e427c691e4288de4f6f95798479
Takeshi Suzuki <tockrock@gmail.com>
2023-10-06 23:15:26 +0900

    Playing with decimals

--- current/e4p.playground/Contents.swift
+++ current/e4p.playground/Contents.swift
@@ -1,3 +1,14 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+var pointOne = Decimal(0.1)
+var pointTwo = pointOne * 2
+
+pointOne + pointTwo
+
+var doubleOne = 0.1
+var doubleTwo = doubleOne * 2.0
+0.1 + 0.2
+
+doubleTwo + doubleOne
+
+doubleOne * 10

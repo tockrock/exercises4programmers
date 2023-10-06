e25a18f7f15fe987b556ce8972f81e32dfebb32f
Takeshi Suzuki <tockrock@gmail.com>
2023-10-06 23:18:14 +0900

    Create new project for exercise 7

---
+++ current/7 Area of a Rectangular Room/Area of a Rectangular Room/Area of a Rectangular Room/Area_of_a_Rectangular_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Area_of_a_Rectangular_RoomApp.swift
+//  Area of a Rectangular Room
+//
+//  Created by 鈴木剛 on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct Area_of_a_Rectangular_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

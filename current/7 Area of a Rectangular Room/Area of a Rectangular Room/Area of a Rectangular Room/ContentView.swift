0a293029119083bf3758a47da92a1c1f0bdef697
Takeshi Suzuki <tockrock@gmail.com>
2023-10-07 23:55:32 +0900

    Added Segmented Picker & cleaned up the visuals

--- current/7 Area of a Rectangular Room/Area of a Rectangular Room/Area of a Rectangular Room/ContentView.swift
+++ current/7 Area of a Rectangular Room/Area of a Rectangular Room/Area of a Rectangular Room/ContentView.swift
@@ -11,30 +11,43 @@ import Observation
 @Observable class RoomModel {
     var lenghtInput = ""
     var widthInput = ""
+    var unitIsMeter: Bool = true
+}
+
+extension RoomModel {
+    private var length: Double? { Double(lenghtInput) }
+    private var width: Double? { Double(widthInput) }
     
-    var length: Double? { Double(lenghtInput) }
-    var width: Double? { Double(widthInput) }
-    
+    private var selectedLengthUnit: UnitLength {
+        unitIsMeter ? UnitLength.meters : UnitLength.feet
+    }
+    private var selectedAreaUnit: UnitArea {
+        unitIsMeter ? UnitArea.squareMeters : UnitArea.squareFeet
+    }
+    private var convertedAreaUnit: UnitArea {
+        unitIsMeter ? UnitArea.squareFeet : UnitArea.squareMeters
+    }
+
     var output: String {
-        guard let length, let width, 
+        guard let length, let width,
               0 < length, 0 < width else {
             return "Please fill positive numbers in the fields above."
         }
 
-        let unitLength = Measurement(value: length, unit: UnitLength.feet)
-        let unitWidth = Measurement(value: width, unit: UnitLength.feet)
+        let unitLength = Measurement(value: length, unit: selectedLengthUnit)
+        let unitWidth = Measurement(value: width, unit: selectedLengthUnit)
         let area = length * width
-        let areaSqFeet = Measurement(value: area, unit: UnitArea.squareFeet)
-        let areaSqMeter = areaSqFeet.converted(to: .squareMeters)
+        let unitArea = Measurement(value: area, unit: selectedAreaUnit)
+        let convertedUnitArea = unitArea.converted(to: convertedAreaUnit)
         
-        return """
-        You entered dimention of \(unitLength) by \(unitWidth).
-        
-        The area is
-        \(areaSqFeet)
-        \(areaSqMeter)
-        """
-        
+        let result = """
+            You entered dimention of \(unitLength) by \(unitWidth).
+            
+            The area is
+            \(unitArea)
+            \(convertedUnitArea)
+            """
+        return result
     }
 }
 
@@ -44,11 +57,25 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Picker("Unit", selection: $model.unitIsMeter) {
+                Text("Meter").tag(true)
+                Text("Feet").tag(false)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.vertical)
+            
             TextField("Length", text: $model.lenghtInput)
             TextField("Width", text: $model.widthInput)
+            
+            Divider()
+                .padding(.vertical)
+            
             Text(model.output)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
         }
-        .padding()
+        .padding(.horizontal)
     }
 }
 

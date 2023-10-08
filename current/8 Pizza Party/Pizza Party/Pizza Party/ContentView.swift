1e90293c8d40b00004b395bdc294ab368eb1c5e4
Takeshi Suzuki <tockrock@gmail.com>
2023-10-08 17:22:15 +0900

    Initial version for Exercise 8 Pizza Party

--- current/8 Pizza Party/Pizza Party/Pizza Party/ContentView.swift
+++ current/8 Pizza Party/Pizza Party/Pizza Party/ContentView.swift
@@ -6,14 +6,73 @@
 //
 
 import SwiftUI
+import Observation
 
-struct ContentView: View {
+
+@Observable class PartyModel {
+    var peopleInput = ""
+    var pizzaInput = ""
+    
+    var peopleCount: Int? { Int(peopleInput) }
+    var pizzaCount: Int? { Int(pizzaInput) }
+    
+    var calculatable: Bool {
+        peopleCount != nil && 
+        pizzaCount != nil &&
+        peopleCount! > 0 &&
+        pizzaCount! > 0
+    }
+    
+    var output: String {
+        guard let peopleCount, let pizzaCount,
+              pizzaCount > 0, pizzaCount > 0 else {
+            return "Please enter positive numbers fields above."
+        }
+        
+        let each = pizzaCount * 8 / peopleCount
+        let leftover = pizzaCount * 8 % peopleCount
+        
+        let output = """
+        \(peopleCount) people with \(pizzaCount) pizzas
+        Each person gets \(each) pieces of pizza.
+        There are \(leftover) leftover piecies.
+        """
+        
+        return output
+    }
+    
+    var peopleDescription: String {
+        guard let people = peopleCount else { return "" }
+        if people == 1 {
+            return "1 person"
+        }
+        return "\(people) people"
+    }
+    
+    var pizzaDescription: String {
+        guard let pizza = pizzaCount else { return "" }
+        if pizza == 1 {
+            return "1 pizza"
+        }
+        return "\(pizza) pizzas"
+    }
+}
+
+struct ContentView {
+    @Bindable var model = PartyModel()
+}
+
+extension ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("People Count", text: $model.peopleInput)
+            TextField("Pizza Count", text: $model.pizzaInput)
+            Divider()
+                .padding(.vertical)
+            Text(model.output)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Spacer()
         }
         .padding()
     }

511944b5c2c32b67a7f2da26698007c1fb459cf4
Takeshi Suzuki <tockrock@gmail.com>
2023-10-08 16:32:44 +0900

    New Project for Exercise 8 Pizza Party

---
+++ current/8 Pizza Party/Pizza Party/Pizza Party/Pizza_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_PartyApp.swift
+//  Pizza Party
+//
+//  Created by 鈴木剛 on 2023/10/08.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
